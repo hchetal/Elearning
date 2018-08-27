7ec8fdedbbe4dafbbaae9f6499f8219295f9af91
Himanshu Chetal <himanshu.chetal@outlook.com>
2018-08-27 22:25:06 +0800

    Commiting custom changes

---
+++ MyName/MyName/Name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace MyName
+{
+    public class Name
+    {
+        public void GetName()
+        {
+            Console.WriteLine("My Name is Himanshu");
+        }
+    }
+}
